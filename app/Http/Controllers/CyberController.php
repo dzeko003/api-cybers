c2f8c116736a75d756c309adf10e1b2745b0d5bb
dzeko003 <berenismassamba@gmail.com>
2024-07-10 19:10:24 +0200

    first commit

---
+++ app/Http/Controllers/CyberController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cyber;
+use Exception;
+use Illuminate\Http\Request;
+
+class CyberController extends Controller
+{
+    public function index()
+    {
+        try {
+            $cybers = Cyber::all();
+            return response()->json($cybers, 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Failed to retrieve cybers',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $cyber = Cyber::findOrFail($id);
+            return response()->json($cyber, 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Failed to retrieve cyber',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'longitude' => 'required|numeric',
+                'latitude' => 'required|numeric',
+                'address' => 'required|string|max:255',
+                'printers' => 'required|integer',
+            ]);
+
+            $cyber = Cyber::create($validatedData);
+
+            return response()->json($cyber, 201);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Failed to create cyber',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function storeBatch(Request $request)
+    {
+        try {
+            $cybersData = $request->json()->all();
+
+            // Tableau pour stocker les instances de Cyber créées
+            $createdCybers = [];
+
+            foreach ($cybersData as $cyberData) {
+                $createdCybers[] = Cyber::create([
+                    'name' => $cyberData['name'],
+                    'longitude' => $cyberData['longitude'],
+                    'latitude' => $cyberData['latitude'],
+                    'address' => $cyberData['address'],
+                    'printers' => $cyberData['printers'],
+                  
+                ]);
+            }
+
+            return response()->json($createdCybers, 201);
+        } catch (\Exception $e) {
+            // Pour toute autre exception non prévue
+            return response()->json(['message' => 'Failed to process request', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+// use App\Http\Controllers\UserController;
+
+use App\Http\Controllers\CyberController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Post
+Route::post('/cybers', [CyberController::class, 'store']);
+Route::post('/cybers/batch', [CyberController::class, 'storeBatch']);
+
+// Get
+Route::get('/cybers', [CyberController::class, 'index']);
+Route::get('/cybers/{id}', [CyberController::class, 'show']);
+ 
+
+
